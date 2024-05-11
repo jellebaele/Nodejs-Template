296b775704ba8045719e10e1601f35dbc987c3d1
Jelle Baele <jellebaele@hotmail.com>
2024-05-12 00:27:11 +0200

    Finalize tsoa

--- src/users/usersController.ts
+++ src/users/usersController.ts
@@ -1,5 +1,16 @@
 // src/users/usersController.ts
-import { Body, Controller, Get, Path, Post, Query, Route, Response, SuccessResponse } from 'tsoa';
+import {
+  Body,
+  Controller,
+  Get,
+  Path,
+  Post,
+  Query,
+  Route,
+  Response,
+  SuccessResponse,
+  Tags,
+} from 'tsoa';
 import { User } from './user';
 import { UsersService, UserCreationParams } from './usersService';
 
@@ -9,7 +20,15 @@ interface ValidateErrorJson {
 }
 
 @Route('users')
+@Tags('User')
 export class UsersController extends Controller {
+  /**
+   * Retrieves the details of an existing user.
+   * Supply the unique user ID from either and receive corresponding user details.
+   * @param userId The user's identifier
+   * @param name Provide a username to display
+   */
+  @Response('404', 'Not found')
   @Get('{userId}')
   public async getUser(@Path() userId: number, @Query() name?: string): Promise<User> {
     return new UsersService().get(userId, name);
